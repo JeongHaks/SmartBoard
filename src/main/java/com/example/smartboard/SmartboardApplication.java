5c3e227842f0388019dc1475c2f5ae7969e3f50b
JeongHaks <challenge0110@naver.com>
2025-08-07 11:14:38 +0900

    사이드프로젝트 start

---
+++ src/main/java/com/example/smartboard/SmartboardApplication.java
@@ -0,0 +1,13 @@
+package com.example.smartboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartboardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartboardApplication.class, args);
+	}
+
+}
